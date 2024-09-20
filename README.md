af7c659d6ed116496e96ddb2a6a747dc8d68620e
pasha <paul@drteam.rocks>
2024-09-20 10:09:37 +0300

    Readme updated

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # LLM MSNP Tests
 
-A small and collection of results, if checks speed of GGUF model on different combination of GPUs and CPUs.  
+A small and collection of results, if checks speed of GGUF model on different combination of GPUs and CPUs.
 
-Inspired due to conversation in Telegram chat.  
+Inspired due to conversation in Telegram chat.
 
 ## MSNP Leaderboards
 
@@ -11,6 +11,15 @@ https://huggingface.co/spaces/evilfreelancer/msnp-leaderboard
 ## How to test
 
 First you need to install [ollama](https://ollama.com/download) to server where you will make tests.
+
+Download models:
+
+```shell
+ollama pull llama3.1:8b-instruct-q4_0
+ollama pull llama3.1:70b-instruct-q4_0
+```
+
+> Please use models with __quantization__ in name
 
 Then you need to create Python Virtual Environment, then chroot to it:
 
@@ -21,13 +30,20 @@ python3 -m venv venv
 source venv/bin/activate
 ```
 
-Then download tests.py file:
+Then download requirements and tests.py files:
 
 ```shell
+wget https://raw.githubusercontent.com/EvilFreelancer/llm-msnp-tests/refs/heads/main/requirements.txt
 wget https://raw.githubusercontent.com/EvilFreelancer/llm-msnp-tests/refs/heads/main/test.py
 ```
 
-And run it:
+Install dependencies:
+
+```shell
+pip install -r requirements.txt
+```
+
+And run test:
 
 ```shell
 python3 test.py 
--- test.py
+++ test.py
@@ -1,41 +1,79 @@
 import requests
 import json
 import timeit
+import re
+import pandas as pd
 
-MODEL = "llama3.1:8b"
-# MODEL = "llama3.1:8b-instruct-fp16"
-# MODEL = "llama3.1:70b"
-# MODEL = "aya:35b-23-q4_K_M"
+# List of models to test
+MODELS = [
+    "llama3.1:8b-instruct-q4_0",
+    "llama3.1:70b-instruct-q4_0",
+    # "aya:35b-23-q4_K_M",
+    # "mistral-small:22b-instruct-2409-q4_0",
+    # "mixtral:8x7b-instruct-v0.1-q4_0"
+]
 
-# num_ctx = 200
-num_ctx = 4000
+# Context length
+# num_ctx = 4000
+num_ctx = 200
 
-start = timeit.default_timer()
-prompt = "С какой периодичностью происходит ледниковый период"
-result = requests.post(
-    'http://gpu02:11434/api/generate',
-    json={
-        'model': MODEL,
-        "options": {
-            "seed": 123,
-            "num_predict": 500,
-            "temperature": 0,
-            "num_ctx": num_ctx
-        },
-        "keep_alive": "10m", "prompt": prompt,
-        "stream": False
+# Results dictionary
+results = []
+
+for MODEL in MODELS:
+    # Start timer
+    start = timeit.default_timer()
+
+    # Prompt to generate text
+    prompt = "С какой периодичностью происходит ледниковый период"
+
+    # Make API request
+    result = requests.post(
+        'http://gpu02:11434/api/generate',
+        json={
+            'model': MODEL,
+            "options": {
+                "seed": 123,
+                "num_predict": 500,
+                "temperature": 0,
+                "num_ctx": num_ctx
+            },
+            "keep_alive": 0,
+            "prompt": prompt,
+            "stream": False
+        }
+    ).content
+
+    # Parse result
+    a = json.loads(result)
+
+    # Calculate duration in seconds
+    nanoseconds = a['eval_duration']
+    seconds = nanoseconds / 1_000_000_000
+
+    # Stop time
+    end = timeit.default_timer()
+
+    # Detect quantization
+    quantization = "q4_0"  # default quantization
+    match = re.search(r'^(.*)-(q\d_.*|fp16)$', MODEL)
+    if match:
+        quantization = match[2]
+        MODEL = match[1]
+
+    results_dict = {
+        'model': MODEL, 'quantization': quantization, 'context_length': num_ctx,
+        'total_tokens': a['eval_count'], 'duration_s': seconds,
+        'tokens_per_sec': a['eval_count'] / seconds, 'time_to_retrieve_s': end - start
     }
-).content
-a = json.loads(result)
-nanoseconds = a['eval_duration']  # 5 billion nanoseconds
-seconds = nanoseconds / 1_000_000_000
-end = timeit.default_timer()
+    # Print intermediate information
+    print(results_dict)
+    results.append(results_dict)
 
-print(a['response'])
-print()
-print(f"Модель: {MODEL}")
-print(f"Размер контекста: {num_ctx}")
-print(f"Всего токенов: {a['eval_count']}")
-print(f"Длительность: {seconds}")
-print(f"Токенов в секунду: {a['eval_count'] / seconds}")
-print(f"Время до получения ответа: {end - start}")
+# df = pandas.DataFrame.from_dict(results, orient='index')
+# print(df.to_markdown(index=False, tablefmt='fancy_grid'))
+
+# Print results table
+df = pd.DataFrame(results)  # .from_dict(results, orient='index')
+print("\n------\n")
+print(df.to_markdown(index=False))
